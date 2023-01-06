7beda012bbc3a0af01800a2248f83b08f3bdd6ee
abc <duranakyol71@gmail.com>
2023-01-06 12:08:52 +0100

    hinzufügen strapi product und review, useFetch

---
+++ React-App/src/components/Review.js
@@ -0,0 +1,55 @@
+import { useState, useCallback } from "react";
+
+export default function Review({ productId }) {
+  const [userDisplayName, setUserDisplayName] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      await fetch("http://localhost:1337/api/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: {
+            userDisplayName: userDisplayName,
+            body: body,
+            product: productId,
+          },
+        }),
+      });
+      setUserDisplayName("");
+      setBody("");
+    },
+    [userDisplayName, body, productId]
+  );
+
+  return (
+    <form
+      className="flex flex-col max-w-md gap-4 mt-10"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={userDisplayName}
+        onChange={(e) => setUserDisplayName(e.currentTarget.value)}
+        className="bg-gray-100 rounded-md p-4"
+      />
+      <textarea
+        placeholder="Review"
+        value={body}
+        onChange={(e) => setBody(e.currentTarget.value)}
+        className="bg-gray-100 rounded-md p-4"
+      ></textarea>
+      <button
+        className="bg-blue-500 p-4 text-white rounded-md self-start"
+        type="submit"
+      >
+        Submit Review
+      </button>
+    </form>
+  );
+}
--- React-App/src/pages/ProductDetails.js
+++ React-App/src/pages/ProductDetails.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import Review from "../components/Review";
 
 function ProductDetails() {
-  return <div>hi! ProductDetails</div>;
+  const { id } = useParams();
+
+  const { isLoading, error, data } = useFetch(
+    "http://localhost:1337/api/products/" + id
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div className="container mx-auto p-12">
+      <h1 className="text-3xl">{data.data.attributes.name}</h1>
+      <code className="text-blue-500 mt-2 block">
+        {data.data.attributes.price.toLocaleString("de-DE", {
+          style: "currency",
+          currency: "EUR",
+        })}
+      </code>
+      <p className="mt-4 text-gray-900">{data.data.attributes.details}</p>
+      <Review productId={id} />
+    </div>
+  );
 }
 
 export default ProductDetails;
